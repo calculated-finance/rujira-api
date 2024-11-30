6c3ac58857be27f844a9c3480bf86f1057b325e6
codehans <94654388+codehans@users.noreply.github.com>
2024-11-30 20:57:58 +0000

    symbol fix

--- lib/rujira/tokens.ex
+++ lib/rujira/tokens.ex
@@ -13,7 +13,7 @@ defmodule Rujira.Tokens do
   def symbol("factory/kujira1tsekaqv9vmem0zwskmf90gpf0twl6k57e8vdnq/urkuji"), do: "rKUJI"
 
   def symbol(x) do
-    case String.split("-") do
+    case String.split(x, "-") do
       [_, a] -> String.upcase(a)
       _ -> String.upcase(x)
     end
