e3e26aa1b630bade720984fa91b19399a23bad33
zefiro <gitlab.pushy576@passmail.net>
2024-12-07 16:51:49 +0100

    lint

--- lib/rujira_web/schema/rujira_types.ex
+++ lib/rujira_web/schema/rujira_types.ex
@@ -4,7 +4,6 @@ defmodule RujiraWeb.Schema.RujiraTypes do
 
   @desc "A rujira represents data about rujira products"
   object :rujira do
-
     field :merge, list_of(:merge_pool) do
       resolve(&Resolvers.Merge.merge_stats/3)
     end
