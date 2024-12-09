4d4e4873f2416f5b348fed94c53fb76904f11eda
zefiro <gitlab.pushy576@passmail.net>
2024-12-09 17:28:09 +0100

    Wip get_rates test

--- test/support/data_mocks/merge.ex
+++ test/support/data_mocks/merge.ex
@@ -3,17 +3,57 @@ defmodule Rujira.DataMocks.Merge do
 
   alias Cosmwasm.Wasm.V1.QuerySmartContractStateResponse
   alias Cosmwasm.Wasm.V1.QuerySmartContractStateRequest
+  alias Cosmwasm.Wasm.V1.QueryContractsByCodeRequest
+  alias Cosmwasm.Wasm.V1.QueryContractsByCodeResponse
+
+  def request(%QueryContractsByCodeRequest{
+        code_id: 31
+      }) do
+    {:ok, :contract_by_code}
+  end
 
   def request(%QuerySmartContractStateRequest{
-        address: "contract-merge",
+        address: "contract-merge-1",
         query_data: "{\"config\":{}}"
       }) do
-    {:ok, :config_response}
+    {:ok, :config_response_1}
+  end
+
+  def request(%QuerySmartContractStateRequest{
+        address: "contract-merge-2",
+        query_data: "{\"config\":{}}"
+      }) do
+    {:ok, :config_response_2}
+  end
+
+  def request(%QuerySmartContractStateRequest{
+        address: "contract-merge-1",
+        query_data: "{\"status\":{}}"
+      }) do
+    {:ok, :status_response_1}
+  end
+
+  def request(%QuerySmartContractStateRequest{
+        address: "contract-merge-2",
+        query_data: "{\"status\":{}}"
+      }) do
+    {:ok, :status_response_2}
   end
 
   def request(_), do: :error
 
-  def response(:config_response) do
+  def response(:contract_by_code) do
+    {:ok,
+     %QueryContractsByCodeResponse{
+       contracts: ["contract-merge-1", "contract-merge-2"],
+       pagination: %Cosmos.Base.Query.V1beta1.PageResponse{
+         next_key: "",
+         total: 2
+       }
+     }}
+  end
+
+  def response(:config_response_1) do
     {:ok,
      %QuerySmartContractStateResponse{
        data:
@@ -24,6 +64,45 @@ defmodule Rujira.DataMocks.Merge do
            ruji_allocation: "10000000000",
            decay_starts_at: "1733424430000000000",
            decay_ends_at: "1764874030000000000"
+         })
+     }}
+  end
+
+  def response(:config_response_2) do
+    {:ok,
+     %QuerySmartContractStateResponse{
+       data:
+         Jason.encode!(%{
+           merge_denom: "gaia-kuji",
+           merge_supply: "2000000000",
+           ruji_denom: "ruji",
+           ruji_allocation: "30000000000",
+           decay_starts_at: "1733424430000000000",
+           decay_ends_at: "1764874030000000000"
+         })
+     }}
+  end
+
+  def response(:status_response_1) do
+    {:ok,
+     %QuerySmartContractStateResponse{
+       data:
+         Jason.encode!(%{
+           merged: "1",
+           shares: "1",
+           size: "2"
+         })
+     }}
+  end
+
+  def response(:status_response_2) do
+    {:ok,
+     %QuerySmartContractStateResponse{
+       data:
+         Jason.encode!(%{
+           merged: "2",
+           shares: "3",
+           size: "4"
          })
      }}
   end
