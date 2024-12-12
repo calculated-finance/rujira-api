75747a4e58cd3794eaeb4ef296eb0936e25501e9
codehans <94654388+codehans@users.noreply.github.com>
2024-12-12 13:30:58 +0000

    fix mode

--- config/config.exs
+++ config/config.exs
@@ -48,4 +48,4 @@ mode = System.get_env("MODE") || "dev"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{MODE}.exs"
+import_config "#{mode}.exs"
