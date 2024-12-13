c36c8afe7d1471c2fa3cb5921b77ad39340f2f85
zefiro <gitlab.pushy576@passmail.net>
2024-12-13 17:28:07 +0100

    WIP simple transaction_event() monitoring

--- lib/rujira/grpc/client.ex
+++ lib/rujira/grpc/client.ex
@@ -2,17 +2,29 @@ defmodule Rujira.Grpc.Client do
   @moduledoc """
   GRPC client with parallel connection attempts to all servers using Poolboy workers.
   """
+  use Appsignal.Instrumentation.Decorators
+
   @timeout 500
 
   def connection do
     :poolboy.transaction(:grpc_pool, &GenServer.call(&1, :channel, @timeout))
   end
 
+  @decorate transaction_event()
   def stub(stub_fn, req) do
     :poolboy.transaction(:grpc_pool, fn worker_pid ->
       try do
+        clean_function_name =
+          stub_fn
+          |> inspect()
+          |> String.trim_leading("&")
+          |> String.replace(".", "_")
+          |> String.replace("/", "_")
+
         with {:ok, channel} <- GenServer.call(worker_pid, :channel, @timeout) do
-          stub_fn.(channel, req)
+          Appsignal.instrument("#{clean_function_name}", fn ->
+            stub_fn.(channel, req)
+          end)
         end
       catch
         :error, _ ->
