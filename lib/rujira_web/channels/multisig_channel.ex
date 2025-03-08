5ec7fb69f9dd4588d7afe599c91281b5f06038c3
codehans <94654388+codehans@users.noreply.github.com>
2025-03-08 14:13:24 +0000

    share sign_response

--- lib/rujira_web/channels/multisig_channel.ex
+++ lib/rujira_web/channels/multisig_channel.ex
@@ -7,13 +7,23 @@ defmodule RujiraWeb.MultisigChannel do
     {:ok, assign(socket, :key, key)}
   end
 
-  def join("multisig:sign:" <> _address, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
+  def join(
+        "multisig:sign:" <> _address,
+        %{"key" => key, "sign_response" => sign_response},
+        socket
+      ) do
+    send(self(), :after_join)
+    {:ok, socket |> assign(:key, key) |> assign(:sign_response, sign_response)}
   end
 
-  def handle_info(:after_join, socket) do
-    {:ok, _} = Presence.track(socket, socket.assigns.key, %{})
+  def handle_info(:after_join, %{assigns: %{key: key}} = socket) do
+    {:ok, _} = Presence.track(socket, key, %{})
+    push(socket, "presence_state", Presence.list(socket))
+    {:noreply, socket}
+  end
 
+  def handle_info(:after_join, %{assigns: %{sign_response: sign_response, key: key}} = socket) do
+    {:ok, _} = Presence.track(socket, key, %{sign_response: sign_response})
     push(socket, "presence_state", Presence.list(socket))
     {:noreply, socket}
   end
