ca3bbfccdda417ba16eaed231f86291958815afa
codehans <94654388+codehans@users.noreply.github.com>
2025-04-28 14:17:49 +0100

    fix reverse

--- lib/rujira/enum.ex
+++ lib/rujira/enum.ex
@@ -1,10 +1,13 @@
 defmodule Rujira.Enum do
   def reduce_while_ok(enum, initial_acc \\ [], fun) when is_function(fun, 1) do
-    Enum.reduce_while(enum, {:ok, initial_acc}, fn element, {:ok, acc} ->
-      case fun.(element) do
-        {:ok, el} -> {:cont, {:ok, acc + el}}
-        {:error, reason} -> {:halt, {:error, reason}}
-      end
-    end)
+    case Enum.reduce_while(enum, {:ok, initial_acc}, fn element, {:ok, acc} ->
+           case fun.(element) do
+             {:ok, el} -> {:cont, {:ok, [el | acc]}}
+             {:error, reason} -> {:halt, {:error, reason}}
+           end
+         end) do
+      {:ok, acc} -> {:ok, Enum.reverse(acc)}
+      {:error, reason} -> {:error, reason}
+    end
   end
 end
