8dc736d29a92289e3b5fdec1e23e21a4f0dea345
zefiro <gitlab.pushy576@passmail.net>
2025-05-14 14:05:06 +0200

    rebase using grpc

--- lib/thorchain/swaps/indexer.ex
+++ lib/thorchain/swaps/indexer.ex
@@ -25,20 +25,15 @@ defmodule Thorchain.Swaps.Indexer do
         },
         state
       ) do
-    with {:ok, %{rune_price_in_tor: price}} <- Thorchain.network(),
-         price <- String.to_integer(price) do
-      index = 2_147_483_647
-      scan_events(height, index, time, events, price)
+    index = 2_147_483_647
+    scan_events(height, index, time, events)
 
-      {:noreply, state}
-    else
-      {:error, _} ->
-        {:noreply, state}
-    end
+    {:noreply, state}
   end
 
-  defp scan_events(height, tx_idx, time, events, price) do
-    swaps = Enum.flat_map(events, &scan_event(&1, price))
+  defp scan_events(height, tx_idx, time, events) do
+    swaps = Enum.flat_map(events, &scan_event(&1))
+    IO.inspect(swaps)
 
     for {swap, idx} <- Enum.with_index(swaps) do
       swap
@@ -52,46 +47,40 @@ defmodule Thorchain.Swaps.Indexer do
     end
   end
 
-  defp scan_event(%{attributes: attributes}, price) do
-    scan_attributes(attributes, price)
+  defp scan_event(%{attributes: attributes, type: "swap"}) do
+    IO.inspect(attributes)
+    scan_attributes(attributes)
   end
 
-  # API returns a list of attributes inside the list of events without key :attributes
-  defp scan_event(attributes, price) when is_map(attributes) do
-    scan_attributes(Rujira.convert_attributes(attributes), price)
-  end
+  defp scan_event(_), do: []
 
-  defp scan_attributes(attributes, price, collection \\ [])
+  defp scan_attributes(attributes, collection \\ [])
 
   defp scan_attributes(
          [
-           %{value: chain, key: "chain"},
-           %{value: coin, key: "coin"},
-           %{value: emit_asset, key: "emit_asset"},
-           %{value: from, key: "from"},
-           %{value: id, key: "id"},
+           %{value: pool, key: "pool"},
+           %{value: _swap_target, key: "swap_target"},
+           %{value: _swap_slip, key: "swap_slip"},
            %{value: _liquidity_fee, key: "liquidity_fee"},
            %{value: liquidity_fee_in_rune, key: "liquidity_fee_in_rune"},
-           %{value: memo, key: "memo"},
-           %{value: _mode, key: "mode"},
-           %{value: pool, key: "pool"},
-           %{value: _pool_slip, key: "pool_slip"},
-           %{value: streaming_swap_count, key: "streaming_swap_count"},
+           %{value: emit_asset, key: "emit_asset"},
            %{value: streaming_swap_quantity, key: "streaming_swap_quantity"},
-           %{value: _swap_slip, key: "swap_slip"},
-           %{value: _swap_target, key: "swap_target"},
-           %{value: to, key: "to"}
+           %{value: streaming_swap_count, key: "streaming_swap_count"},
+           %{value: _pool_slip, key: "pool_slip"},
+           %{value: id, key: "id"},
+           %{value: chain, key: "chain"},
+           %{value: from, key: "from"},
+           %{value: to, key: "to"},
+           %{value: coin, key: "coin"},
+           %{value: memo, key: "memo"}
            | rest
          ],
-         price,
          collection
        ) do
     scan_attributes(
       rest,
-      price,
       insert_swap(
         collection,
-        price,
         pool,
         liquidity_fee_in_rune,
         emit_asset,
@@ -107,23 +96,32 @@ defmodule Thorchain.Swaps.Indexer do
     )
   end
 
-  defp scan_attributes([_ | rest], price, collection) do
-    scan_attributes(rest, price, collection)
+  defp scan_attributes([_ | rest], collection) do
+    scan_attributes(rest, collection)
   end
 
-  defp scan_attributes([], _, collection), do: collection
+  defp scan_attributes([], collection), do: collection
 
   defp get_affiliate_data(memo, price, volume_usd) do
     case Thorchain.get_affiliate(memo) do
       {:ok, {aff, bps}} ->
-        affiliate_fee =
-          Decimal.new(volume_usd) |> Decimal.mult(bps) |> Decimal.round() |> Decimal.to_integer()
+        affiliate_fee_in_usd =
+          volume_usd
+          |> Decimal.mult(bps)
+          |> Decimal.round()
+          |> Decimal.to_integer()
+
+        affiliate_fee_in_rune =
+          affiliate_fee_in_usd
+          |> Decimal.div(price)
+          |> Decimal.round()
+          |> Decimal.to_integer()
 
         %{
           affiliate: aff,
           affiliate_bps: bps,
-          affiliate_fee_in_rune: Prices.normalize(affiliate_fee / price, 4),
-          affiliate_fee_in_usd: affiliate_fee
+          affiliate_fee_in_rune: affiliate_fee_in_rune,
+          affiliate_fee_in_usd: affiliate_fee_in_usd
         }
 
       {:error, :no_affiliate} ->
@@ -133,7 +131,6 @@ defmodule Thorchain.Swaps.Indexer do
 
   defp insert_swap(
          collection,
-         price,
          pool,
          liquidity_fee_in_rune,
          emit_asset,
@@ -146,48 +143,53 @@ defmodule Thorchain.Swaps.Indexer do
          coin,
          memo
        ) do
-    liquidity_fee_in_rune = String.to_integer(liquidity_fee_in_rune)
-    liquidity_fee_in_usd = Prices.normalize(liquidity_fee_in_rune * price, 16)
-    streaming_swap_quantity = String.to_integer(streaming_swap_quantity)
-    streaming_swap_count = String.to_integer(streaming_swap_count)
+    with {:ok, %{price: price}} <- Prices.get("RUNE") do
+      liquidity_fee_in_rune = String.to_integer(liquidity_fee_in_rune)
+      streaming_swap_quantity = String.to_integer(streaming_swap_quantity)
+      streaming_swap_count = String.to_integer(streaming_swap_count)
 
-    [coin_amount, coin_asset] = String.split(coin, " ", parts: 2)
-    [emit_asset_amount, emit_asset_asset] = String.split(emit_asset, " ", parts: 2)
-    coin_amount = String.to_integer(coin_amount)
-    emit_asset_amount = String.to_integer(emit_asset_amount)
+      [coin_amount, coin_asset] = String.split(coin, " ", parts: 2)
+      [emit_asset_amount, emit_asset_asset] = String.split(emit_asset, " ", parts: 2)
+      coin_amount = String.to_integer(coin_amount)
+      emit_asset_amount = String.to_integer(emit_asset_amount)
 
-    volume_usd =
-      cond do
-        String.contains?(coin_asset, "THOR.RUNE") ->
-          Prices.normalize(coin_amount * price, 16)
+      liquidity_fee_in_usd =
+        liquidity_fee_in_rune
+        |> Decimal.mult(price)
+        |> Decimal.round()
+        |> Decimal.to_integer()
 
-        String.contains?(emit_asset_asset, "THOR.RUNE") ->
-          Prices.normalize(emit_asset_amount * price, 16)
+      volume_usd =
+        swap_size_rune({coin_asset, coin_amount}, {emit_asset_asset, emit_asset_amount})
+        |> Decimal.mult(price)
+        |> Decimal.round()
+        |> Decimal.to_integer()
 
-        true ->
-          0
-      end
+      swap =
+        %{
+          pool: pool,
+          liquidity_fee_in_rune: liquidity_fee_in_rune,
+          emit_asset_asset: emit_asset_asset,
+          emit_asset_amount: emit_asset_amount,
+          streaming_swap_quantity: streaming_swap_quantity,
+          streaming_swap_count: streaming_swap_count,
+          id: id,
+          chain: chain,
+          from: from,
+          to: to,
+          coin_asset: coin_asset,
+          coin_amount: coin_amount,
+          memo: memo,
+          volume_usd: volume_usd,
+          liquidity_fee_in_usd: liquidity_fee_in_usd
+        }
+        |> Map.merge(get_affiliate_data(memo, price, volume_usd))
 
-    swap =
-      %{
-        pool: pool,
-        liquidity_fee_in_rune: liquidity_fee_in_rune,
-        emit_asset_asset: emit_asset_asset,
-        emit_asset_amount: emit_asset_amount,
-        streaming_swap_quantity: streaming_swap_quantity,
-        streaming_swap_count: streaming_swap_count,
-        id: id,
-        chain: chain,
-        from: from,
-        to: to,
-        coin_asset: coin_asset,
-        coin_amount: coin_amount,
-        memo: memo,
-        volume_usd: volume_usd,
-        liquidity_fee_in_usd: liquidity_fee_in_usd
-      }
-      |> Map.merge(get_affiliate_data(memo, price, volume_usd))
-
-    [swap | collection]
+      [swap | collection]
+    end
   end
+
+  defp swap_size_rune({"THOR.RUNE", in_amount}, _), do: in_amount
+  defp swap_size_rune(_, {"THOR.RUNE", out_amount}), do: out_amount
+  defp swap_size_rune(_, _), do: 0
 end
