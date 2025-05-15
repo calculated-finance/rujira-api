7f6033f60bd9b83c9845c580c2bab8cde98d33df
codehans <94654388+codehans@users.noreply.github.com>
2025-05-15 17:28:58 +0100

    stagenet ids

--- config/prod.stagenet.exs
+++ config/prod.stagenet.exs
@@ -1,2 +1,5 @@
 import Config
-config :rujira, Rujira.Merge, code_ids: []
+
+config :rujira, Rujira.Bow, code_ids: [8]
+config :rujira, Rujira.Fin, code_ids: [9]
+config :rujira, Rujira.Merge, code_ids: [2]
