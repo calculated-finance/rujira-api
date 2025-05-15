3e856fa7f3e89c45aec3460b91759d491254b00d
codehans <94654388+codehans@users.noreply.github.com>
2025-05-15 17:34:13 +0100

    fin ids

--- config/prod.stagenet.exs
+++ config/prod.stagenet.exs
@@ -1,5 +1,5 @@
 import Config
 
 config :rujira, Rujira.Bow, code_ids: [8]
-config :rujira, Rujira.Fin, code_ids: [9]
+config :rujira, Rujira.Fin, pair_code_ids: [9]
 config :rujira, Rujira.Merge, code_ids: [2]
