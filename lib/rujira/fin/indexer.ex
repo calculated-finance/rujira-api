f728b562071269086c2af2eb4aa637b3ba6b3cb2
codehans <94654388+codehans@users.noreply.github.com>
2025-05-15 16:15:39 +0100

    insert order

--- lib/rujira/fin/indexer.ex
+++ lib/rujira/fin/indexer.ex
@@ -20,21 +20,21 @@ defmodule Rujira.Fin.Indexer do
         },
         state
       ) do
-    for %{hash: txhash, result: %{events: events}} <- txs do
-      scan_events(height, txhash, events, time)
+    for {%{hash: txhash, result: %{events: events}}, tx_idx} <- Enum.with_index(txs) do
+      scan_events(height, txhash, tx_idx, events, time)
     end
 
     {:noreply, state}
   end
 
-  defp scan_events(height, txhash, events, time) do
+  defp scan_events(height, txhash, tx_idx, events, time) do
     events
     |> Enum.flat_map(&scan_event/1)
-    |> Enum.reverse()
     |> Enum.with_index()
     |> Enum.map(fn {trade, idx} ->
       Map.merge(trade, %{
         height: height,
+        tx_idx: tx_idx,
         idx: idx,
         txhash: txhash,
         timestamp: time,
@@ -59,25 +59,23 @@ defmodule Rujira.Fin.Indexer do
            %{key: "offer", value: offer},
            %{key: "price", value: price},
            %{key: "rate", value: rate},
-           %{key: "side", value: side},
-           %{key: "msg_index", value: msg_index}
+           %{key: "side", value: side}
            | rest
          ],
          collection
        ) do
     scan_attributes(
       rest,
-      insert_trade(collection, contract_address, rate, price, offer, bid, msg_index, side)
+      insert_trade(collection, contract_address, rate, price, offer, bid, side)
     )
   end
 
   defp scan_attributes([_ | rest], collection), do: scan_attributes(rest, collection)
   defp scan_attributes([], collection), do: collection
 
-  defp insert_trade(collection, contract_address, rate, price, offer, bid, msg_index, side) do
+  defp insert_trade(collection, contract_address, rate, price, offer, bid, side) do
     with {offer, _} <- Integer.parse(offer),
          {bid, _} <- Integer.parse(bid),
-         {tx_idx, _} <- Integer.parse(msg_index),
          {rate, _} <- Float.parse(rate) do
       if offer > 100 && bid > 100 do
         trade = %{
@@ -86,8 +84,7 @@ defmodule Rujira.Fin.Indexer do
           bid: bid,
           rate: rate,
           price: price,
-          side: String.to_existing_atom(side),
-          tx_idx: tx_idx
+          side: String.to_existing_atom(side)
         }
 
         [trade | collection]
