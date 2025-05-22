12dc9567892d5e217199e69819c719023ea655a6
zefiro <gitlab.pushy576@passmail.net>
2025-05-22 22:13:09 +0200

    add invalidation of fin pair on bow deposit/withdraw

--- lib/rujira/fin/listener.ex
+++ lib/rujira/fin/listener.ex
@@ -1,5 +1,6 @@
 defmodule Rujira.Fin.Listener do
   alias Rujira.Fin
+  alias Rujira.Bow
   use GenServer
   require Logger
 
@@ -20,20 +21,33 @@ defmodule Rujira.Fin.Listener do
   end
 
   defp scan_txs(txs) do
-    addresses =
+    events =
       txs
       |> Enum.flat_map(fn
         %{result: %{events: xs}} when is_list(xs) -> xs
         _ -> []
       end)
+
+    addresses =
+      events
       |> Enum.flat_map(&scan_event/1)
       |> Enum.uniq()
       # Trades are withdrawn before retraction. Ensure we're not re-publishing an order that doens't exist
       |> Enum.reverse()
 
+    bow_addresses = events |> Enum.flat_map(&scan_bow_event/1) |> Enum.uniq()
+
     for address <- addresses |> Enum.map(&elem(&1, 1)) |> Enum.uniq() do
       Memoize.invalidate(Fin, :query_book, [address, :_])
       id = Absinthe.Relay.Node.to_global_id(:fin_book, address, RujiraWeb.Schema)
+      Absinthe.Subscription.publish(RujiraWeb.Endpoint, %{id: id}, node: id)
+    end
+
+    for {address, pair} <- bow_addresses do
+      Memoize.invalidate(Fin, :query_book, [pair, :_])
+      Memoize.invalidate(Fin, :query_orders, [pair, address, :_, :_])
+
+      id = Absinthe.Relay.Node.to_global_id(:fin_book, pair, RujiraWeb.Schema)
       Absinthe.Subscription.publish(RujiraWeb.Endpoint, %{id: id}, node: id)
     end
 
@@ -77,5 +91,20 @@ defmodule Rujira.Fin.Listener do
     price = Map.get(map, "price")
     owner = Map.get(map, "owner")
     [{name, address, owner, side, price}]
+  end
+
+  def scan_bow_event(%{attributes: attributes, type: "wasm-rujira-bow/" <> _}) do
+    scan_bow_attributes(attributes)
+  end
+
+  def scan_bow_event(_), do: []
+
+  defp scan_bow_attributes(attributes) do
+    map = Map.new(attributes, fn %{key: k, value: v} -> {k, v} end)
+    address = Map.get(map, "_contract_address")
+
+    with {:ok, pair} <- Bow.fin_pair(address) do
+      [{address, pair}]
+    end
   end
 end
