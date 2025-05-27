5bc7e227e994958656f275f20db9d6b3b5b17191
codehans <94654388+codehans@users.noreply.github.com>
2025-05-27 15:15:59 +0100

    refactor

--- lib/rujira/events.ex
+++ lib/rujira/events.ex
@@ -1,12 +1,15 @@
 defmodule Rujira.Events do
+  @schema RujiraWeb.Schema
+  @endpoint RujiraWeb.Endpoint
+
   def publish_node(type, id) do
-    id = Absinthe.Relay.Node.to_global_id(type, id, RujiraWeb.Schema)
-    Absinthe.Subscription.publish(RujiraWeb.Endpoint, %{id: id}, node: id)
+    id = Absinthe.Relay.Node.to_global_id(type, id, @schema)
+    Absinthe.Subscription.publish(@endpoint, %{id: id}, node: id)
   end
 
   def publish_edge(type, prefix, id) do
-    id = Absinthe.Relay.Node.to_global_id(type, id, RujiraWeb.Schema)
-    prefix = Absinthe.Relay.Node.to_global_id(type, prefix, RujiraWeb.Schema)
-    Absinthe.Subscription.publish(RujiraWeb.Endpoint, %{id: id}, node: id, edge: prefix)
+    id = Absinthe.Relay.Node.to_global_id(type, id, @schema)
+    prefix = Absinthe.Relay.Node.to_global_id(type, prefix, @schema)
+    Absinthe.Subscription.publish(@endpoint, %{id: id}, node: id, edge: prefix)
   end
 end
