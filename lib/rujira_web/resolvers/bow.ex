9d7de0bb425f1a3086b4370e5ece0bf5a3785dda
codehans <94654388+codehans@users.noreply.github.com>
2025-05-28 11:49:23 +0100

    fix book pair

--- lib/rujira_web/resolvers/bow.ex
+++ lib/rujira_web/resolvers/bow.ex
@@ -27,8 +27,9 @@ defmodule RujiraWeb.Resolvers.Bow do
   end
 
   def quotes(%{address: address}, _, _) do
-    with {:ok, quotes} <- Rujira.Bow.load_quotes(address) do
-      {:ok, Map.put(quotes, :contract, address)}
+    with {:ok, pair} <- Rujira.Bow.fin_pair(address),
+         {:ok, quotes} <- Rujira.Bow.load_quotes(address) do
+      {:ok, Map.put(quotes, :contract, pair.address)}
     end
   end
 end
