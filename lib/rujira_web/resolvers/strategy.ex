04a0a1f11263c163a42375109ae17ca0097f2007
codehans <94654388+codehans@users.noreply.github.com>
2025-06-04 09:47:00 +0100

    lint

--- lib/rujira_web/resolvers/strategy.ex
+++ lib/rujira_web/resolvers/strategy.ex
@@ -49,7 +49,7 @@ defmodule RujiraWeb.Resolvers.Strategy do
     end
   end
 
-  defp thorchain_query(query, %{asset: %{symbol: "TCY"}}), do: false
+  defp thorchain_query(_, %{asset: %{symbol: "TCY"}}), do: false
 
   defp thorchain_query(query, %{asset: %{chain: "THOR"} = asset}) do
     Assets.query_match(query, asset, Assets.from_string("THOR.RUNE"))
