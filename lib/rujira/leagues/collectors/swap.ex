9d96ea0b6ad39d42de05f224a7618adec9689d2d
codehans <94654388+codehans@users.noreply.github.com>
2025-06-04 10:21:20 +0100

    use tor price for swap pricing

--- lib/rujira/leagues/collectors/swap.ex
+++ lib/rujira/leagues/collectors/swap.ex
@@ -65,7 +65,7 @@ defmodule Rujira.Leagues.Collectors.Swap do
 
   defp league_event(asset, memo, fee_amount, tx_id) do
     with {:ok, address} <- Thorchain.get_dest_address(memo),
-         {:ok, %{current: price, change: _}} <- Prices.get(asset) do
+         {:ok, %{current: price}} <- Prices.tor_price(asset) do
       affiliate_fee =
         fee_amount
         |> Decimal.mult(price)
