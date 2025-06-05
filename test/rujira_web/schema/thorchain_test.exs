1689bd400513af41790c7fb25e7e9d19d946d9ed
codehans <94654388+codehans@users.noreply.github.com>
2025-06-05 17:00:15 +0100

    pool test

--- test/rujira_web/schema/thorchain_test.exs
+++ test/rujira_web/schema/thorchain_test.exs
@@ -1,46 +1,106 @@
 defmodule RujiraWeb.Schema.ThorchainTest do
   use RujiraWeb.ConnCase
 
-  @query """
-  query thorchain {
-    thorchainV2 {
-      pools {
-        asset {
-          asset
-        }
-        assetTorPrice
-        balanceAsset
-        balanceRune
-        decimals
-        derivedDepthBps
-        id
-        loanCollateral
-        loanCollateralRemaining
-        loanCr
-        lpUnits
-        pendingInboundAsset
-        pendingInboundRune
-        poolUnits
-        saversCapacityRemaining
-        saversDepth
-        saversFillBps
-        saversUnits
-        shortCode
-        status
-        synthMintPaused
-        synthSupply
-        synthSupplyRemaining
-        synthUnits
-      }
+  @pool_fragment """
+  fragment ThorchainPoolFragment on ThorchainPool {
+    asset {
+      asset
     }
+    assetTorPrice
+    balanceAsset
+    balanceRune
+    decimals
+    derivedDepthBps
+    id
+    loanCollateral
+    loanCollateralRemaining
+    loanCr
+    lpUnits
+    pendingInboundAsset
+    pendingInboundRune
+    poolUnits
+    saversCapacityRemaining
+    saversDepth
+    saversFillBps
+    saversUnits
+    shortCode
+    status
+    synthMintPaused
+    synthSupply
+    synthSupplyRemaining
+    synthUnits
   }
   """
 
-  test "query: pools", %{conn: conn} do
+  @query """
+  query {
+    thorchainV2 {
+      pools {
+        ...ThorchainPoolFragment
+      }
+    }
+  }
+  #{@pool_fragment}
+  """
+
+  test "pools", %{conn: conn} do
     conn = post(conn, "/api", %{"query" => @query})
     res = json_response(conn, 200)
     assert Map.get(res, "errors") == nil
     %{"data" => %{"thorchainV2" => %{"pools" => pools}}} = res
     assert Enum.count(pools) > 0
+  end
+
+  @query """
+  query($id: ID!) {
+    node(id: $id) {
+      ... on ThorchainPool {
+        ...ThorchainPoolFragment
+      }
+    }
+  }
+  #{@pool_fragment}
+  """
+
+  test "pool", %{conn: conn} do
+    conn =
+      post(conn, "/api", %{
+        "query" => @query,
+        "variables" => %{"id" => Base.encode64("ThorchainPool:BTC.BTC")}
+      })
+
+    res = json_response(conn, 200)
+    assert Map.get(res, "errors") == nil
+
+    %{
+      "data" => %{
+        "node" => %{
+          "asset" => %{"asset" => "BTC.BTC"},
+          "assetTorPrice" => _,
+          "balanceAsset" => _,
+          "balanceRune" => _,
+          "decimals" => _,
+          "derivedDepthBps" => _,
+          "id" => _,
+          "loanCollateral" => _,
+          "loanCollateralRemaining" => _,
+          "loanCr" => _,
+          "lpUnits" => _,
+          "pendingInboundAsset" => _,
+          "pendingInboundRune" => _,
+          "poolUnits" => _,
+          "saversCapacityRemaining" => _,
+          "saversDepth" => _,
+          "saversFillBps" => _,
+          "saversUnits" => _,
+          "shortCode" => _,
+          "status" => _,
+          "synthMintPaused" => _,
+          "synthSupply" => _,
+          "synthSupplyRemaining" => _,
+          "synthUnits" => _
+        }
+      }
+    } = res
   end
 end
