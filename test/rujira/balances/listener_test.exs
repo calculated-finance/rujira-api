5139fabbed48b4dc781c99f17189c6ba0d4df7f6
zefiro <gitlab.pushy576@passmail.net>
2025-06-06 19:11:17 +0200

    rm test listner WIP

--- test/rujira/balances/listener_test.exs
+++ test/rujira/balances/listener_test.exs
@@ -3,15 +3,15 @@ defmodule Rujira.Balances.ListenerTest do
   import Mox
   setup :verify_on_exit!
 
-  test "publishes account update" do
-    expect(Rujira.Events.PublisherMock, :publish, fn endpoint, payload, topics ->
-      nil
-      # assert endpoint == RujiraWeb.Endpoint
-      # assert topics == [node: Base.encode64("Layer1Account:thor:foo")]
-      # assert payload == %{id: Base.encode64("Layer1Account:thor:foo")}
-    end)
+  # test "publishes account update" do
+  #   expect(Rujira.Events.PublisherMock, :publish, fn endpoint, payload, topics ->
+  #     nil
+  #     # assert endpoint == RujiraWeb.Endpoint
+  #     # assert topics == [node: Base.encode64("Layer1Account:thor:foo")]
+  #     # assert payload == %{id: Base.encode64("Layer1Account:thor:foo")}
+  #   end)
 
-    {:ok, block} = Thorchain.block("")
-    Rujira.Balances.Listener.handle_info(block, nil)
-  end
+  #   {:ok, block} = Thorchain.block("")
+  #   Rujira.Balances.Listener.handle_info(block, nil)
+  # end
 end
