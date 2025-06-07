cc358a4ac148545751a4d30ee57aa6398618a1b6
zefiro <gitlab.pushy576@passmail.net>
2025-06-07 16:47:25 +0200

    load fixture and fragments at compiler time

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,11 +2,3 @@ ExUnit.start()
 Mox.defmock(Rujira.Events.PublisherMock, for: Rujira.Events.Publisher)
 Application.put_env(:rujira, Rujira.Events, publisher: Rujira.Events.PublisherMock)
 Ecto.Adapters.SQL.Sandbox.mode(Rujira.Repo, :manual)
-
-# Load all fixture modules under test/fixtures/
-Path.wildcard(Path.expand("fixtures/**/*.ex", __DIR__))
-|> Enum.each(&Code.require_file/1)
-
-# Load all fragment modules under test/rujira_web/fragments/
-Path.wildcard(Path.expand("rujira_web/fragments/**/*.ex", __DIR__))
-|> Enum.each(&Code.require_file/1)
