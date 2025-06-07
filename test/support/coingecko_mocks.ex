09e49fcc6f21832dcb0bb553e5782296e41abddf
zefiro <gitlab.pushy576@passmail.net>
2025-06-07 17:20:35 +0200

    expand coingecko mocks

--- test/support/coingecko_mocks.ex
+++ test/support/coingecko_mocks.ex
@@ -3,14 +3,19 @@ defmodule Rujira.CoingeckoMocks do
   import ExUnit.Assertions
 
   @mock_body_price %{
-    "tcy"     => %{"usd" => 42.0,     "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000},
+    "tcy" => %{"usd" => 42.0, "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000},
     "bitcoin" => %{"usd" => 100_000.0, "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000},
-    "rune"    => %{"usd" => 100.0,    "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000}
+    "thorchain" => %{"usd" => 100.0, "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000},
+    "nami-protocol" => %{"usd" => 100.0, "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000},
+    "mantadao" => %{"usd" => 100.0, "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000},
+    "usd-coin" => %{"usd" => 1.0, "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000},
+    "bifrost-bridged-bnb-bifrost" => %{"usd" => 230, "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000},
+    "bifrost-bridged-eth-bifrost" => %{"usd" => 1000, "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000},
+    "tether" => %{"usd" => 1.0, "usd_24h_change" => 1.23, "usd_market_cap" => 1_000_000},
   }
-
-  @mock_body_search_by_id %{
-    "RUNE" => %{"coins" => [%{"id" => "rune"}]}
-  }
+  
+  # fetch from https://api.coingecko.com/api/v3/coins/list
+  @coins_list File.read!("test/fixtures/coin_list") |> Jason.decode!()
 
   @doc """
   Install a global Tesla mock for all Coingecko endpoints.
@@ -25,9 +30,18 @@ defmodule Rujira.CoingeckoMocks do
           %Tesla.Env{env | status: 200, body: @mock_body_price}
 
         path == "/api/v3/search" ->
-          symbol = params["query"]
-          body   = Map.get(@mock_body_search_by_id, symbol, %{"coins" => []})
-          %Tesla.Env{env | status: 200, body: body}
+          symbol = params["query"] |> String.downcase()
+
+          case Enum.find(@coins_list, fn %{"symbol" => sym} ->
+                 String.downcase(sym) == symbol
+               end) do
+            %{"id" => id} ->
+              body = %{"coins" => [%{"id" => id}]}
+              %Tesla.Env{env | status: 200, body: body}
+
+            nil ->
+              %Tesla.Env{env | status: 200, body: %{"coins" => []}}
+          end
 
         true ->
           flunk("unexpected HTTP call to #{url}")
