1034fb0e593983d301ca98cf0d289495ba4c7389
zefiro <gitlab.pushy576@passmail.net>
2025-06-11 16:16:28 +0200

    add test on order created and filled

--- test/rujira/fin/listener_test.exs
+++ test/rujira/fin/listener_test.exs
@@ -65,4 +65,70 @@ defmodule Rujira.Fin.ListenerTest do
               ]}
            ]
   end
+
+  test "published events on Order Created and Order Filled" do
+    # 4574056 executes a Fin Order Created on the ruji - rune pair
+    {:ok, block} = Block.load_block("4574056")
+    Rujira.Fin.Listener.handle_new_block(block, nil)
+
+    messages = collect_publishes()
+    assert length(messages) == 2
+
+    assert messages == [
+             {:published, RujiraWeb.Endpoint,
+              %{
+                id:
+                  Base.encode64(
+                    "FinBook:sthor1knzcsjqu3wpgm0ausx6w0th48kvl2wvtqzmvud4hgst4ggutehlseele4r"
+                  )
+              },
+              [
+                node:
+                  Base.encode64(
+                    "FinBook:sthor1knzcsjqu3wpgm0ausx6w0th48kvl2wvtqzmvud4hgst4ggutehlseele4r"
+                  )
+              ]},
+             {:published, RujiraWeb.Endpoint,
+              %{
+                price: "fixed:1.012",
+                side: "base",
+                contract: "sthor1knzcsjqu3wpgm0ausx6w0th48kvl2wvtqzmvud4hgst4ggutehlseele4r"
+              },
+              [
+                fin_order_updated:
+                  "sthor1t4gsjfs8q8j3mw2e402r8vzrtaslsf5re3ktut"
+              ]}
+           ]
+    # fill the order on block 4574088
+    {:ok, block} = Block.load_block("4574088")
+    Rujira.Fin.Listener.handle_new_block(block, nil)
+
+    filled_messages = collect_publishes()
+    assert length(filled_messages) == 2
+
+    assert filled_messages == [
+             {:published, RujiraWeb.Endpoint,
+              %{
+                id:
+                  Base.encode64(
+                    "FinBook:sthor1knzcsjqu3wpgm0ausx6w0th48kvl2wvtqzmvud4hgst4ggutehlseele4r"
+                  )
+              },
+              [
+                node:
+                  Base.encode64(
+                    "FinBook:sthor1knzcsjqu3wpgm0ausx6w0th48kvl2wvtqzmvud4hgst4ggutehlseele4r"
+                  )
+              ]},
+             {:published, RujiraWeb.Endpoint,
+              %{
+                price: "fixed:1.012",
+                side: "base"
+              },
+              [
+                fin_order_filled:
+                  "sthor1knzcsjqu3wpgm0ausx6w0th48kvl2wvtqzmvud4hgst4ggutehlseele4r/base/fixed:1.012"
+              ]}
+           ]
+  end
 end
