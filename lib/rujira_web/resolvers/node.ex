46f235578e37e21f92c6a60c75ee0445c1f45554
codehans <94654388+codehans@users.noreply.github.com>
2025-06-26 11:33:05 +0100

    nullable node

--- lib/rujira_web/resolvers/node.ex
+++ lib/rujira_web/resolvers/node.ex
@@ -38,6 +38,8 @@ defmodule RujiraWeb.Resolvers.Node do
     resolve_id(encoded_id)
   end
 
+  def id(_, _), do: {:ok, nil}
+
   @doc """
   Resolves multiple nodes by their global IDs in parallel.
 
