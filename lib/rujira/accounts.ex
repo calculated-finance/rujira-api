eaacf0f28d2e43ce189dffac3469b42f922b0787
codehans <94654388+codehans@users.noreply.github.com>
2025-07-18 14:57:01 +0100

    rescue

--- lib/rujira/accounts.ex
+++ lib/rujira/accounts.ex
@@ -10,8 +10,8 @@ defmodule Rujira.Accounts do
 
     try do
       {:ok, %Layer1{id: id, chain: String.to_existing_atom(chain), address: address}}
-    catch
-      _ ->
+    rescue
+      ArgumentError ->
         {:error, :invalid_chain}
     end
   end
