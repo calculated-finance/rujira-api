f347206c61ff395698d006848cb0f84c3cf08791
codehans <94654388+codehans@users.noreply.github.com>
2025-07-19 16:20:06 +0100

    ids

--- lib/rujira/fin/listener.ex
+++ lib/rujira/fin/listener.ex
@@ -7,12 +7,14 @@ defmodule Rujira.Fin.Listener do
   use Supervisor
 
   def start_link(_) do
-    # Start individual listeners for each pair/event type
-    pair_children()
-    |> Enum.concat(thorchain_children())
-    |> Enum.concat(order_children())
-    |> Enum.concat(bow_children())
-    |> Supervisor.start_link(strategy: :one_for_one)
+    with {:ok, pairs} <- pair_children(),
+         {:ok, orders} <- order_children(),
+         {:ok, bows} <- bow_children(),
+         {:ok, thorchains} <- thorchain_children() do
+      [pairs, orders, bows, thorchains]
+      |> Enum.concat()
+      |> Supervisor.start_link(strategy: :one_for_one)
+    end
   end
 
   @impl true
@@ -23,33 +25,31 @@ defmodule Rujira.Fin.Listener do
   # Creates Thorchain oracle listeners for pairs with oracle configuration
   defp thorchain_children do
     with {:ok, pairs} <- Rujira.Fin.list_pairs() do
-      pairs
-      |> Enum.filter(&(not is_nil(&1.oracle_base) and not is_nil(&1.oracle_quote)))
-      |> Enum.map(&Supervisor.child_spec({__MODULE__.Thorchain, &1}, id: &1.id))
+      {:ok,
+       pairs
+       |> Enum.filter(&(not is_nil(&1.oracle_base) and not is_nil(&1.oracle_quote)))
+       |> Enum.map(&Supervisor.child_spec({__MODULE__.Thorchain, &1}, id: "Thorchain" <> &1.id))}
     end
   end
 
   # Creates trade listeners to update order books when trades occur
   defp pair_children do
     with {:ok, pairs} <- Rujira.Fin.list_pairs() do
-      pairs
-      |> Enum.map(&Supervisor.child_spec({__MODULE__.Pair, &1}, id: &1.id))
+      {:ok, Enum.map(pairs, &Supervisor.child_spec({__MODULE__.Pair, &1}, id: "Pair" <> &1.id))}
     end
   end
 
   # Creates order event listeners to track order placements/cancellations
   defp order_children do
     with {:ok, pairs} <- Rujira.Fin.list_pairs() do
-      pairs
-      |> Enum.map(&Supervisor.child_spec({__MODULE__.Order, &1}, id: &1.id))
+      {:ok, Enum.map(pairs, &Supervisor.child_spec({__MODULE__.Order, &1}, id: "Order" <> &1.id))}
     end
   end
 
   # Creates Bow market maker listeners to update books for MM actions
   defp bow_children do
     with {:ok, pools} <- Rujira.Bow.list_pools() do
-      pools
-      |> Enum.map(&Supervisor.child_spec({__MODULE__.Bow, &1}, id: &1.id))
+      {:ok, Enum.map(pools, &Supervisor.child_spec({__MODULE__.Bow, &1}, id: "Bow" <> &1.id))}
     end
   end
 end
