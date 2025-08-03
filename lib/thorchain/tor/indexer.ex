8054de24c9681efbaf77e4edb71622b4ab93bfb9
codehans <94654388+codehans@users.noreply.github.com>
2025-08-03 16:27:24 +0100

    index rune tor price

--- lib/thorchain/tor/indexer.ex
+++ lib/thorchain/tor/indexer.ex
@@ -11,12 +11,20 @@ defmodule Thorchain.Tor.Indexer do
   @impl true
   def handle_new_block(%{header: %{height: height, time: time}}, state) do
     with {:ok, pools} <- Thorchain.pools(height),
-         {:ok, time} <- DateTime.from_naive(time, "Etc/UTC") do
+         {:ok, time} <- DateTime.from_naive(time, "Etc/UTC"),
+         {:ok, rune} <- rune_price() do
       pools
       |> Enum.map(&{&1.asset.id, &1.asset_tor_price, time})
+      |> Enum.concat([{"THOR.RUNE", rune, time}])
       |> Thorchain.Tor.update_candles()
     end
 
     {:noreply, state}
+  end
+
+  def rune_price() do
+    with {:ok, %{rune_price_in_tor: price}} <- Thorchain.network() do
+      {:ok, String.to_integer(price)}
+    end
   end
 end
