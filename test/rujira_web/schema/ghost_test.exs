a8a976e881cfa61ead46c7475aeae49ce3f75b2d
codehans <94654388+codehans@users.noreply.github.com>
2025-08-13 11:01:51 +0100

    fix tests

--- test/rujira_web/schema/ghost_test.exs
+++ test/rujira_web/schema/ghost_test.exs
@@ -2,6 +2,8 @@ defmodule RujiraWeb.Schema.GhostTest do
   use RujiraWeb.ConnCase
   import RujiraWeb.Fragments.GhostFragments
 
+  import Tesla.Mock
+
   @strategies_query """
   query {
     strategies(first: 1, typenames:["GhostVault"]) {
@@ -27,6 +29,15 @@ defmodule RujiraWeb.Schema.GhostTest do
   """
 
   test "list, and lookup vault", %{conn: conn} do
+    mock(fn
+      %{
+        method: :get,
+        url:
+          "https://indexer-mainnet.levana.finance/v2/markets-earn-data?network=rujira-mainnet&factory=thor1gclfrvam6a33yhpw3ut3arajyqs06esdvt9pfvluzwsslap9p6uqt4rzxs"
+      } ->
+        %Tesla.Env{status: 200, body: %{}}
+    end)
+
     # 1) fetch all vaults
     conn = post(conn, "/api", %{"query" => @strategies_query})
     %{"data" => %{"strategies" => %{"edges" => edges}}} = res = json_response(conn, 200)
