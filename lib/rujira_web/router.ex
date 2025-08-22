478677fedd77dc6b7783fc30251dbdc310ccb6f8
codehans <94654388+codehans@users.noreply.github.com>
2025-08-22 07:24:56 +0200

    dev dashboard

--- lib/rujira_web/router.ex
+++ lib/rujira_web/router.ex
@@ -30,18 +30,19 @@ defmodule RujiraWeb.Router do
   end
 
   # Enable LiveDashboard in development
-  if Application.compile_env(:rujira, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
+  # if Application.compile_env(:rujira, :dev_routes) do
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  import Phoenix.LiveDashboard.Router
 
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
+  scope "/dev" do
+    pipe_through [:fetch_session, :protect_from_forgery]
 
-      live_dashboard "/dashboard", metrics: RujiraWeb.Telemetry
-    end
+    live_dashboard "/dashboard", metrics: RujiraWeb.Telemetry
   end
+
+  # end
 end
