f16edd26e0b9c2868e02d5f6964130a0507152c3
zefiro <gitlab.pushy576@passmail.net>
2025-08-27 11:10:45 +0200

    feat: add coin

---
+++ lib/rujira/assets/coin.ex
@@ -0,0 +1,8 @@
+defmodule Rujira.Assets.Coin do
+  @moduledoc false
+  defstruct [:denom, :amount]
+
+  def parse(%{"denom" => denom, "amount" => amount}) do
+    {:ok, %__MODULE__{denom: denom, amount: amount}}
+  end
+end
