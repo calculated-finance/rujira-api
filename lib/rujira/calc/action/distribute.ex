69aa66ac337db11c51b2f1414a8eb66347110a38
zefiro <gitlab.pushy576@passmail.net>
2025-08-27 14:19:23 +0200

    feat: implement distribute

--- lib/rujira/calc/action/distribute.ex
+++ lib/rujira/calc/action/distribute.ex
@@ -3,10 +3,18 @@ defmodule Rujira.Calc.Action.Distribute do
   Action type for distributing assets to multiple recipients.
   Currently a placeholder implementation.
   """
-  defstruct []
-  # TODO: implement from_config
 
-  def from_config(_) do
-    {:ok, %__MODULE__{}}
+  defstruct denoms: [],
+            destinations: []
+
+  @type t :: %__MODULE__{
+          denoms: [String.t()],
+          destinations: [Destination.t()]
+        }
+
+  def from_config(%{"denoms" => denoms, "destinations" => destinations}) do
+    with {:ok, denoms} <- Rujira.Enum.reduce_while_ok(denoms, &Destination.from_config/1) do
+      {:ok, %__MODULE__{denoms: denoms, destinations: destinations}}
+    end
   end
 end
