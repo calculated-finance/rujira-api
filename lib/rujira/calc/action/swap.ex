4c23bae1db185f773c3e0a21ddd7fe5515e83da0
zefiro <gitlab.pushy576@passmail.net>
2025-08-27 14:02:40 +0200

    feat: implement swap action

--- lib/rujira/calc/action/swap.ex
+++ lib/rujira/calc/action/swap.ex
@@ -1,12 +1,38 @@
 defmodule Rujira.Calc.Action.Swap do
   @moduledoc """
   Action type for executing token swaps between different denominations.
-  Currently a placeholder implementation.
   """
-  defstruct []
+  alias Rujira.Assets.Coin
+  alias Rujira.Calc.Common.SwapAmountAdjustment
+  alias Rujira.Calc.Common.SwapRoute
 
-  # TODO: implement from_config
-  def from_config(_) do
-    {:ok, %__MODULE__{}}
+  defstruct [
+    swap_amount: Coin.default(),
+    minimum_receive_amount: Coin.default(),
+    maximum_slippage_bps: 0,
+    adjustment: SwapAmountAdjustment.default(),
+    routes: []
+  ]
+
+  def from_config(%{
+        "swap_amount" => swap_amount,
+        "minimum_receive_amount" => minimum_receive_amount,
+        "maximum_slippage_bps" => maximum_slippage_bps,
+        "adjustment" => adjustment,
+        "routes" => routes
+      }) do
+    with {:ok, swap_amount} <- Coin.parse(swap_amount),
+         {:ok, minimum_receive_amount} <- Coin.parse(minimum_receive_amount),
+         {:ok, adjustment} <- SwapAmountAdjustment.from_config(adjustment),
+         {:ok, routes} <- Rujira.Enum.reduce_while_ok(routes, &SwapRoute.from_config/1) do
+      {:ok,
+       %__MODULE__{
+         swap_amount: swap_amount,
+         minimum_receive_amount: minimum_receive_amount,
+         maximum_slippage_bps: maximum_slippage_bps,
+         adjustment: adjustment,
+         routes: routes
+       }}
+    end
   end
 end
